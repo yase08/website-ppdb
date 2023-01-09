db986c8a6008d50a1feeb2bdf24011da45fc334d
yase08 <ayasbogor@gmail.com>
2023-01-09 14:55:03 +0700

    update

--- resources/views/admin/payment.blade.php
+++ resources/views/admin/payment.blade.php
@@ -29,13 +29,113 @@
                                         <label>Nama Rekening Bank</label>
                                         <select class="form-control" name="bank_name" id="nama_bank" onchange="show()">
                                             <option></option>
-                                            <option>esempe 1</option>
-                                            <option>esempe 2</option>
-                                            <option>esempe 3</option>
-                                            <option>esempe 4</option>
-                                            <option>esempe 5</option>
-                                            <option>esempe 6</option>
-                                            <option>esempe 7</option>
+                                            <option>PT BANK RAKYAT INDONESIA</option>
+                                            <option>PT BANK MANDIRI</option>
+                                            <option>PT BANK NEGARA INDONESIA</option>
+                                            <option>PT BANK TABUNGAN NEGARA</option>
+                                            <option>PT BANK DANAMON INDONESIA</option>
+                                            <option>PT BANK PERMATA</option>
+                                            <option>PT BANK CENTRAL ASIA</option>
+                                            <option>PT BANK MAYBANK  INDONESIA</option>
+                                            <option>PT PAN INDONESIA BANK</option>
+                                            <option>PT BANK CIMB NIAGA</option>
+                                            <option>PT BANK UOB INDONESIA</option>
+                                            <option>PT BANK OCBC NISP</option>
+                                            <option>PT BANK ARTHA GRAHA INTERNASIONAL</option>
+                                            <option>PT BANK BUMI ARTA</option>
+                                            <option>PT BANK HSBC INDONESIA</option>
+                                            <option>PT BANK JTRUST INDONESIA</option>
+                                            <option>PT BANK MAYAPADA INTERNATIONAL</option>
+                                            <option>PT BANK OF INDIA INDONESIA</option>
+                                            <option>PT BANK MUAMALAT INDONESIA</option>
+                                            <option>PT BANK MESTIKA DHARMA</option>
+                                            <option>PT BANK SHINHAN INDONESIA</option>
+                                            <option>PT BANK SINARMAS</option>
+                                            <option>PT BANK MASPION INDONESIA</option>
+                                            <option>PT BANK GANESHA</option>
+                                            <option>PT BANK ICBC INDONESIA</option>
+                                            <option>PT BANK QNB INDONESIA</option>
+                                            <option>PT BANK WOORI SAUDARA INDONESIA 1906</option>
+                                            <option>PT BANK MEGA</option>
+                                            <option>PT BANK KB BUKOPIN</option>
+                                            <option>PT BANK SYARIAH INDONESIA</option>
+                                            <option>PT BANK KEB HANA INDONESIA</option>
+                                            <option>PT BANK MNC INTERNASIONAL</option>
+                                            <option>PT BANK RAYA INDONESIA</option>
+                                            <option>PT BANK SBI INDONESIA</option>
+                                            <option>PT BANK MEGA SYARIAH</option>
+                                            <option>PT BANK INDEX SELINDO</option>
+                                            <option>PT BANK MAYORA</option>
+                                            <option>PT BANK CHINA CONSTRUCTION BANK INDONESIA</option>
+                                            <option>PT BANK DBS INDONESIA</option>
+                                            <option>PT BANK RESONA PERDANIA</option>
+                                            <option>PT BANK MIZUHO INDONESIA</option>
+                                            <option>PT BANK CAPITAL INDONESIA</option>
+                                            <option>PT BANK BNP PARIBAS INDONESIA</option>
+                                            <option>PT BANK ANZ INDONESIA</option>
+                                            <option>PT BANK IBK INDONESIA</option>
+                                            <option>PT BANK ALADIN SYARIAH</option>
+                                            <option>PT BANK CTBC INDONESIA</option>
+                                            <option>PT BANK COMMONWEALTH</option>
+                                            <option>PT BANK BTPN</option>
+                                            <option>PT BANK VICTORIA SYARIAH</option>
+                                            <option>PT BANK JABAR BANTEN SYARIAH</option>
+                                            <option>PT KROM BANK INDONESIA</option>
+                                            <option>PT BANK JASA JAKARTA</option>
+                                            <option>PT BANK NEO COMMERCE</option>
+                                            <option>PT BANK DIGITAL BCA</option>
+                                            <option>PT BANK NATIONALNOBU</option>
+                                            <option>PT BANK INA PERDANA</option>
+                                            <option>PT BANK PANIN DUBAI SYARIAH</option>
+                                            <option>PT PRIMA MASTER BANK</option>
+                                            <option>PT BANK KB BUKOPIN SYARIAH</option>
+                                            <option>PT BANK SAHABAT SAMPOERNA</option>
+                                            <option>PT BANK OKE INDONESIA</option>
+                                            <option>PT BANK AMAR INDONESIA</option>
+                                            <option>PT BANK SEABANK INDONESIA</option>
+                                            <option>PT BANK BCA SYARIAH</option>
+                                            <option>PT BANK JAGO TBK</option>
+                                            <option>PT BANK BTPN SYARIAH</option>
+                                            <option>PT BANK MULTIARTA SENTOSA</option>
+                                            <option>PT BANK FAMA INTERNASIONAL</option>
+                                            <option>PT BANK MANDIRI TASPEN</option>
+                                            <option>PT BANK VICTORIA INTERNATIONAL</option>
+                                            <option>PT ALLO BANK INDONESIA</option>
+                                            <option>PT BPD JAWA BARAT DAN BANTEN</option>
+                                            <option>PT BPD DKI</option>
+                                            <option>PT BPD DAERAH ISTIMEWA YOGYAKARTA</option>
+                                            <option>PT BPD JAWA TENGAH</option>
+                                            <option>PT BPD JAWA TIMUR</option>
+                                            <option>PT BPD JAMBI</option>
+                                            <option>PT BANK ACEH SYARIAH</option>
+                                            <option>PT BPD SUMATERA UTARA</option>
+                                            <option>PT BANK NAGARI</option>
+                                            <option>PT BPD RIAU KEPRI SYARIAH</option>
+                                            <option>PT BPD SUMATERA SELATAN DAN BANGKA BELITUNG</option>
+                                            <option>PT BPD LAMPUNG</option>
+                                            <option>PT BPD KALIMANTAN SELATAN</option>
+                                            <option>PT BPD KALIMANTAN BARAT</option>
+                                            <option>PT BPD KALIMANTAN TIMUR DAN KALIMANTAN UTARA</option>
+                                            <option>PT BPD KALIMANTAN TIMUR DAN KALIMANTAN UTARA</option>
+                                            <option>PT BPD SULAWESI SELATAN DAN SULAWESI BARAT</option>
+                                            <option>PT BPD SULAWESI UTARA DAN  GORONTALO</option>
+                                            <option>PT BANK NTB SYARIAH</option>
+                                            <option>PT BPD BALI</option>
+                                            <option>PT BPD NUSA TENGGARA TIMUR</option>
+                                            <option>PT BPD MALUKU DAN MALUKU UTARA</option>
+                                            <option>PT BPD PAPUA</option>
+                                            <option>PT BPD BENGKULU</option>
+                                            <option>PT BPD SULAWESI TENGAH</option>
+                                            <option>PT BPD SULAWESI TENGGARA</option>
+                                            <option>PT BPD BANTEN</option>
+                                            <option>CITIBANK N.A.</option>
+                                            <option>JP MORGAN CHASE BANK NA</option>
+                                            <option>BANK OF AMERICA N.A</option>
+                                            <option>BANGKOK BANK PCL</option>
+                                            <option>MUFG BANK LTD</option>
+                                            <option>STANDARD CHARTERED BANK</option>
+                                            <option>DEUTSCHE BANK AG</option>
+                                            <option>BANK OF CHINA (HONG KONG) LIMITED CABANG JAKARTA</option>
                                             <option value="lainnya">lainnya</option>
                                         </select>
                                     </div>
